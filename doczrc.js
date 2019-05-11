6f52bb2b8cda68224adf71760039330679bc8f46
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-05-11 12:22:37 -0300

    docs: add IBM Plex font family

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
   codeSandbox: false,
   description: 'IBM\'s Carbon Design System as standalone, styled React components.',
+  indexHtml: './source/index.html',
   menu: ['Home'],
   title: 'Styled Carbon React'
 }
