8d66c2f7b4aadb494d5863133ad7413bad20407b
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-06-23 16:01:48 -0300

    component: use outline on Button

--- source/Button/Button.style.js
+++ source/Button/Button.style.js
@@ -4,7 +4,7 @@ const baseButtonStyle = () => css`
   align-items: center;
   background-color: #0062ff;
   border-radius: 0;
-  border: 1px solid transparent;
+  border: 3px solid transparent;
   box-sizing: border-box;
   color: #fff;
   cursor: pointer;
@@ -16,56 +16,30 @@ const baseButtonStyle = () => css`
   justify-content: space-between;
   letter-spacing: .16px;
   line-height: 18px;
-  margin: 0;
+  margin: 8px;
   max-width: 320px;
   min-height: 48px;
-  outline: 3px solid transparent;
+  outline-offset: -4px;
+  outline: 1px solid transparent;
   padding: 14px 63px 14px 15px;
   position: relative;
   text-align: left;
   text-decoration: none;
   text-rendering: optimizeLegibility;
-  transition: 70ms cubic-bezier(0, 0, .38, .9);
-
-  &:before {
-    border: 1px solid transparent;
-    box-sizing: border-box;
-    content: "";
-    height: calc(100% - 4px);
-    left: 2px;
-    position: absolute;
-    top: 2px;
-    transition: 70ms cubic-bezier(0,0,.38,.9);
-    width: calc(100% - 4px);
-  }
-
-  &:after {
-    border: 3px solid transparent;
-    box-sizing: border-box;
-    content: "";
-    height: calc(100% + 2px);
-    left: -1px;
-    position: absolute;
-    top: -1px;
-    transition: 70ms cubic-bezier(0,0,.38,.9);
-    width: calc(100% + 2px);
-  }
-
-  &:hover {
-    color: #fff;
-    background-color: #0353e9;
-  }
+  transition: all 70ms cubic-bezier(0, 0, .38, .9);
 
   &:active {
     background-color: #0530ad;
   }
 
-  &:focus:before {
-    border-color: #fff;
+  &:focus {
+    border-color: #0062ff;
+    outline-color: #fff;
   }
 
-  &:focus:after {
-    border-color: #0062ff;
+  &:hover {
+    background-color: #0353e9;
+    color: #fff;
   }
 `
 
